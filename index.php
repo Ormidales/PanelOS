2e65de41707b6d11a31baa34d104e956eac866d7
ormidales <hugodoueil@gmail.com>
2023-02-13 10:36:21 +0100

    Modifications 13/02/2023 à 10h36

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PanelOS - v0.0.1a</title>
+</head>
+<body>
+    <h1>PanelOS - v0.0.1a</h1>
+    <h2>PanelOS is a web-based operating system for your server.</h2>
+    <h3>PanelOS is currently in development, and is not ready for use.</h3>
+    <h4>PanelOS is currently in development by</h4>
+</body>
+</html>
