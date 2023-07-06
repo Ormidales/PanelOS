3c004547441ff07be63803913b4f885bd5767ded
Hugo Doueil <xdocontact@gmail.com>
2023-07-06 12:13:39 +0200

    Modifications 06/07/2023

--- javascript/windowProperties.js
+++ javascript/windowProperties.js
@@ -9,3 +9,15 @@ function window_exempleWindow_fermer(){
     document.getElementById("window_exempleWindow").style.opacity = "0";
     document.getElementById("window_exempleWindow").style.transition = "opacity 200ms, visibility 0ms 200ms";
 }
+
+// WINDOW - TEST CARD
+function window_testCard_ouvrir(){
+    document.getElementById("window_testCard").style.visibility = "visible";
+    document.getElementById("window_testCard").style.opacity = "1";
+    document.getElementById("window_testCard").style.transition = "opacity 200ms";
+}
+function window_testCard_fermer(){
+    document.getElementById("window_testCard").style.visibility = "hidden";
+    document.getElementById("window_testCard").style.opacity = "0";
+    document.getElementById("window_testCard").style.transition = "opacity 200ms, visibility 0ms 200ms";
+}
